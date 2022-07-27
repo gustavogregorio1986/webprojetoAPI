45047b9c17dc2c2bf64250db705861e02f642d4b
guga56 <gustavogregorio086@gmail.com>
2022-07-27 12:37:04 -0300

    projeto incial

---
+++ WebprojetoAPI.Dominio/Dominio/Endereco.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebprojetoAPI.Dominio.Dominio
+{
+    public class Endereco
+    {
+        public int IdEndereco { get; set; }
+
+        public string Logradouro { get; set; }
+
+        public string Complemento { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Uf { get; set; }
+
+        public Pessoa Pessoa { get; set; }
+
+        public int Id_Pessoa { get; set; }
+    }
+}
---
+++ WebprojetoAPI.Dominio/Dominio/Processo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebprojetoAPI.Dominio.Dominio
+{
+    public class Processo
+    {
+        public int IdProcesso { get; set; }
+
+        public string NumProcesso { get; set; }
+
+        public string NomeProcesso { get; set; }
+
+        public Pessoa Pessoa { get; set; }
+
+        public int Id_Pessoa { get; set; }
+    }
+}
